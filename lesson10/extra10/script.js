b83a1edead92857d6004e1e5d05be81d7a946c76
gauzeee <gauzeee@gmail.com>
2018-10-19 15:15:14 +0300

    lesson10fix1

--- lesson10/extra10/script.js
+++ lesson10/extra10/script.js
@@ -26,36 +26,29 @@ let input = document.querySelector(".tel"),
     matrix = "+7 (___) ___-__-__";
 input.value = matrix;
 
-function setCursorPosition(pos, elem) {
-    elem.focus();
-    if (elem.setSelectionRange) { 
-        elem.setSelectionRange(pos, pos);
-    } else if (elem.createTextRange) {
-        var range = elem.createTextRange();
-        range.collapse(true);
-        range.moveEnd("character", pos);
-        range.moveStart("character", pos);
-        range.select();
-    }
-}
- 
 function mask(event) {
-    i = 0,
-    def = matrix.replace(/\D/g, ""),
-    val = this.value.replace(/\D/g, "");
-    
-    if (def.length >= val.length) { 
+    let i = 0,
+        def = matrix.replace(/\D/g, ""),
+        val = this.value.replace(/\D/g, "");
+
+     if (def.length >= val.length) { 
         val = def;
-    }
+    } 
     this.value = matrix.replace(/./g, function(a) {
-        return /[_\d]/.test(a) && i < val.length ? val.charAt(i++) : i >= val.length ? "" : a
+        if (/[_\d]/.test(a) && i < val.length) {
+           return val.charAt(i++);
+        } else {
+            if (i >= val.length) {
+               return '';
+            } else {
+                return a;
+            }
+        }
     });
     if (event.type == "blur") {
-        if (this.value.length == 2) {
-            this.value = "";
+        if (this.value.length <= 2) {
+            this.value = matrix;
         }
-    } else { 
-        setCursorPosition(this.value.length, this);
     }
 }
 
