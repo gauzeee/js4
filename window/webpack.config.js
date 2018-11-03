965131fa9882cfb768cdf5d30956f33d1d24676a
gauzeee <gauzeee@gmail.com>
2018-11-03 23:11:14 +0300

    change webpack mode

--- window/webpack.config.js
+++ window/webpack.config.js
@@ -1,41 +1,42 @@
-'use strict';
+"use strict";
 
-let path = require('path');
-const UglifyJsPlugin = require('uglifyjs-webpack-plugin');
+let path = require("path");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
 
 module.exports = {
-    mode: 'development',
-    entry: './src/index.js',
-    watch: true,
+  mode: "production",
+  entry: "./src/index.js",
+  watch: true,
+  target: "node",
+  devtool: "source-map",
 
-    devtool: "source-map",
-
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /(node_modules|bower_components)/,
-                use: {
-                    loader: 'babel-loader?optional[]=runtime',
-                    options: {
-                        presets: [
-                            ["@babel/env", {
-                                targets: {
-                                    edge: "17",
-                                    firefox: "60",
-                                    chrome: "67",
-                                    safari: "11.1",
-                                    ie: "11"
-                                },
-                                useBuiltIns: "usage"
-                            }]
-                        ]
-                    }
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: "babel-loader?optional[]=runtime",
+          options: {
+            presets: [
+              [
+                "@babel/env",
+                {
+                  targets: {
+                    edge: "17",
+                    firefox: "60",
+                    chrome: "67",
+                    safari: "11.1",
+                    ie: "11"
+                  },
+                  useBuiltIns: "usage"
                 }
-            }
-        ]
-    },
-    plugins: [
-        new UglifyJsPlugin()
+              ]
+            ]
+          }
+        }
+      }
     ]
+  },
+  plugins: [new UglifyJsPlugin()]
 };
